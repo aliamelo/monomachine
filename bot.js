3beca381c41610df593fd5bd35931595a33d014a
alia <malo.lecomte@epita.fr>
2020-01-02 14:56:34 +0100

    [BASE] [FIX] - changed token

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
-const Discord = require("discord.js");
+const Discord = require("./dependencies/node_modules/discord.js");
 const client = new Discord.Client();
 
-
+var token = "NjYyMjgxNDM4ODEyNzAwNjky.Xg316A.y_d5Xuqzv1uYo8NhLpy23PtbpSE";
 
 client.on("ready", () => {
     console.log("Logged in as ${client.user.tag}!");
 });
 
-client.login("662281438812700692");
+client.login(token);
