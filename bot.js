b2abcf6a3b695df0a0d34569d90b719c557ad8eb
alia <malo.lecomte@epita.fr>
2020-01-02 16:27:44 +0100

    [BASE] [ADD] - add items

--- bot.js
+++ bot.js
@@ -4,9 +4,15 @@ const client = new Discord.Client();
 const fs = require("fs");
 const ids = JSON.parse(fs.readFileSync("data/ids.json"));
 
+const items = require("./src/items.js");
 
 client.on("ready", () => {
     console.log("Monomachine started");
+});
+
+client.on("message", msg => {
+    if (msg.content == "bip")
+        items.display(msg.channel);
 });
 
 client.login(ids.token);
