ccee8ec78e1f52f330822b7b444ae22262e31eb8
alia <malo.lecomte@epita.fr>
2020-01-02 23:43:20 +0100

    [ITEMS] [MODIFY] - changed exports and display func

--- src/items.js
+++ src/items.js
@@ -1,10 +1,27 @@
 const fs = require("fs");
 const item_list = JSON.parse(fs.readFileSync("data/items.json"));
 
-module.exports = {
-    display: function(channel) {
+function display(channel)
+{
+    var items_str = "";
 
-        for (var item in item_list)
-            channel.send(item_list[item]);
+    for (var item in item_list)
+    {
+        if (item)
+            items_str += ", ";
+
+        items_str += item_list[item];
     }
+
+    channel.send(items_str);
+}
+
+function add_items(argc, argv)
+{
+    console.log("boup");
+}
+
+module.exports = {
+    display: display
+    add_items: add_items
 }
